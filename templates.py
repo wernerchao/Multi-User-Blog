b36a5a7a07de66d5965f6d878de3d6c5b11683bd
Werner Chao <oahcrenrew@gmail.com>
2016-11-24 17:19:59 +0800

    added user check if login for post request for form submission

--- templates.py
+++ templates.py
@@ -279,22 +279,26 @@ class NewPostHandler(Handler):
             self.render('signup.html', error_general="Please signup or login")
 
     def post(self):
-        title = self.request.get("title")
-        content = self.request.get("content")
-        liked_by = [self.user.name]
+        if self.user:
+            title = self.request.get("title")
+            content = self.request.get("content")
+            liked_by = [self.user.name]
 
-        if title and content:
-            p = Post(title=title,
-                     content=content,
-                     created_by=self.user.name,
-                     likes=0,
-                     liked_by=liked_by)
-            p.put()
-            self.redirect('/blog/%s' % str(p.key().id()))
+            if title and content:
+                p = Post(title=title,
+                        content=content,
+                        created_by=self.user.name,
+                        likes=0,
+                        liked_by=liked_by)
+                p.put()
+                self.redirect('/blog/%s' % str(p.key().id()))
+            else:
+                error = "We need both title and content"
+                self.render("newpost.html", title=title,
+                            content=content, error=error)
         else:
-            error = "We need both title and content"
-            self.render("newpost.html", title=title,
-                        content=content, error=error)
+            # self.response.out.write("You need to login!")
+            self.render('signup.html', error_general="Please signup or login")
 
 
 # Handles the delete button click to delete the specific post.
